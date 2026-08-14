decd7e72d4cad6196de5e1db8d9ff7e9a5756a5f
shannon bentley <314344808+uciuscpa-design@users.noreply.github.com>
2026-08-14 16:20:02 -0700

    gexy: export local Greek fallback utilities

--- packages/gexy/__init__.py
+++ packages/gexy/__init__.py
@@ -1,6 +1,13 @@
 """GEXY option-surface normalization and hedge-exposure analytics."""
 
 from packages.gexy.exposure import build_gex_surface, contract_exposure
+from packages.gexy.greeks import (
+    EuropeanOptionGreeks,
+    GreekEnrichment,
+    black_scholes_greeks,
+    enrich_missing_greeks,
+    implied_volatility_from_price,
+)
 from packages.gexy.models import (
     GexContribution,
     GexStrikeLevel,
@@ -12,13 +19,18 @@ from packages.gexy.models import (
 from packages.gexy.normalization import normalize_alpaca_option_surface
 
 __all__ = [
+    "EuropeanOptionGreeks",
     "GexContribution",
     "GexStrikeLevel",
     "GexSurface",
+    "GreekEnrichment",
     "NormalizedOptionSurface",
     "OptionSurfacePoint",
     "OptionType",
+    "black_scholes_greeks",
     "build_gex_surface",
     "contract_exposure",
+    "enrich_missing_greeks",
+    "implied_volatility_from_price",
     "normalize_alpaca_option_surface",
 ]
