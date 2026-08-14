05fc70b73a4d4f03520a9266e53fc203d53502f8
shannon bentley <314344808+uciuscpa-design@users.noreply.github.com>
2026-08-14 16:23:40 -0700

    gexy: export surface pipeline and level analytics

--- packages/gexy/__init__.py
+++ packages/gexy/__init__.py
@@ -8,6 +8,7 @@ from packages.gexy.greeks import (
     enrich_missing_greeks,
     implied_volatility_from_price,
 )
+from packages.gexy.levels import GexWallSummary, rank_levels_by_unsigned_gex, summarize_gex_walls
 from packages.gexy.models import (
     GexContribution,
     GexStrikeLevel,
@@ -17,20 +18,27 @@ from packages.gexy.models import (
     OptionType,
 )
 from packages.gexy.normalization import normalize_alpaca_option_surface
+from packages.gexy.pipeline import GexySurfaceResult, GreekSourceCounts, build_enriched_gexy_surface
 
 __all__ = [
     "EuropeanOptionGreeks",
     "GexContribution",
     "GexStrikeLevel",
     "GexSurface",
+    "GexWallSummary",
+    "GexySurfaceResult",
     "GreekEnrichment",
+    "GreekSourceCounts",
     "NormalizedOptionSurface",
     "OptionSurfacePoint",
     "OptionType",
     "black_scholes_greeks",
+    "build_enriched_gexy_surface",
     "build_gex_surface",
     "contract_exposure",
     "enrich_missing_greeks",
     "implied_volatility_from_price",
     "normalize_alpaca_option_surface",
+    "rank_levels_by_unsigned_gex",
+    "summarize_gex_walls",
 ]
